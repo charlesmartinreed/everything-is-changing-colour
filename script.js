1ebfe867d93d05c6b699fe39b47dea3c058dc24d
charlesmartinreed <charlesmartinreed@icloud.com>
2019-12-11 19:16:55 -0600

    Add loadTrack function to play different audio tracks.

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 const audio = document.querySelector("audio");
 audio.controls = true;
 
+const tracks = [
+  {
+    trackNumber: 1,
+    trackTitle: "Dueling Auroras",
+    trackArtist: "Lone",
+    albumTitle: "Everything Is Changing Colour",
+    src: "./assets/track_1.mp3"
+  },
+  {
+    trackNumber: 2,
+    trackTitle: "Proper Vivid",
+    trackArtist: "Lone",
+    albumTitle: "Everything Is Changing Colour",
+    src: "./assets/track_2.mp3"
+  }
+];
+
+let currentTrackID = 1;
+// let currentTrack = tracks[currentTrackID];
+
 let songDuration;
 const playerToggle = document.querySelector(".play-toggle");
 
@@ -8,10 +28,38 @@ let songNameEl = document.querySelector(".song-name");
 let artistNameEl = document.querySelector(".artist-name");
 let albumNameEl = document.querySelector(".album-name");
 
+let songList = document.querySelectorAll(".track-item");
+
 window.addEventListener("DOMContentLoaded", () => {
-  songNameEl.textContent = audio.dataset.songName;
-  artistNameEl.textContent = audio.dataset.artistName;
-  albumNameEl.textContent = audio.dataset.albumName;
+  loadTrack(currentTrackID);
+
+  songList.forEach(song => {
+    if (song.dataset.trackNumber === String(currentTrackID)) {
+      song.classList.add("playing");
+    }
+  });
+});
+
+loadTrack = tracknum => {
+  let track = tracks[tracknum - 1];
+
+  songNameEl.textContent = track.trackTitle;
+  artistNameEl.textContent = track.trackArtist;
+  albumNameEl.textContent = track.albumTitle;
+
+  audio.src = track.src;
+  audio.play();
+};
+
+songList.forEach(song => {
+  song.addEventListener("click", () => {
+    songList.forEach(song => song.classList.remove("playing"));
+
+    song.classList.add("playing");
+
+    currentTrackID = song.dataset.trackNumber;
+    loadTrack(currentTrackID);
+  });
 });
 
 audio.addEventListener("playing", () => {
